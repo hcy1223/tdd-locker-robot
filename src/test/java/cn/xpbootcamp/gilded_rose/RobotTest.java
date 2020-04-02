622564e8bcaea5fab8774ee8cb8578c7f52afe4e
hcy1223 <chenyuan.hu@thoughtworks.com>
2020-04-02 21:46:35 +0800

    should_throw_exception_when_store_given_2_lockers_first_0_capacity_second_0_capacity

--- src/test/java/cn/xpbootcamp/gilded_rose/RobotTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/RobotTest.java
@@ -1,5 +1,6 @@
 package cn.xpbootcamp.gilded_rose;
 
+import cn.xpbootcamp.gilded_rose.exception.NoEmptyLockersException;
 import cn.xpbootcamp.gilded_rose.locker.Bag;
 import cn.xpbootcamp.gilded_rose.locker.Locker;
 import cn.xpbootcamp.gilded_rose.locker.Robot;
@@ -8,8 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import static cn.xpbootcamp.gilded_rose.locker.Locker.createLocker;
 import static java.util.Arrays.asList;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RobotTest {
 
@@ -25,5 +25,17 @@ public class RobotTest {
         assertNotNull(ticket);
         assertEquals(firstLocker.getEmptyCount(), 0);
         assertEquals(secondLocker.getEmptyCount(), 2);
+    }
+
+    @Test
+    void should_throw_exception_when_store_given_2_lockers_first_0_capacity_second_0_capacity() {
+        Locker firstLocker = createLocker(0);
+        Locker secondLocker = createLocker(0);
+        Robot robot = new Robot(asList(firstLocker, secondLocker));
+        Bag bag = new Bag();
+
+        assertThrows(NoEmptyLockersException.class, () -> {
+            Ticket ticket = robot.store(bag);
+        });
     }
 }
